29184cb5db4dbb4b39b5e17c3a7d09d37ffbfc90
rathin kothiya <rathinkothiya@gmail.com>
2018-12-11 22:39:26 -0600

    echofile

---
+++ final echo proejct/Assets/movingobject.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class movingobject : MonoBehaviour
+{
+    AudioSource audioData;
+    AudioEchoFilter echofilter;
+    float temp4,temp5,temp6,value2;
+    int value1;
+    public List<float> distance;
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        echofilter = GetComponent<AudioEchoFilter>();
+    }
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * 0.01f * 150.0f;
+        var z = Input.GetAxis("Vertical") * 0.01f * 10.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            
+            StartCoroutine(Example());
+            
+            temp4 = rathin2.temp1;
+            temp5 = rathin1.temp2;
+            temp6 = newrathin.temp3;
+             distance= new List<float>();
+            distance.Add(temp4);
+            distance.Add(temp5);
+            distance.Add(temp6);
+           
+            distance.Sort();
+            int counter1 = 0;
+
+            for (int i = 0;i<3 ; i++) {
+
+                if (distance[i] <= 34.3)
+                {
+                }
+                else
+                {
+
+                    value1 = Mathf.RoundToInt(distance[i] * 1000 / 343);
+
+                    GetComponent<AudioEchoFilter>().delay = value1;
+                    value2 = distance[i];
+                    value2 = (float)value2 / (float)49.314/(float)100;
+                    value2 = (float)0.5 - value2;
+
+                    GetComponent<AudioEchoFilter>().decayRatio = value2;
+                    counter1 = counter1 + 1;
+                    print(distance[i]);
+                    audioData.Play(0);
+                    
+                }
+            }
+            if (counter1 == 0)
+            {
+                echofilter.enabled = false;
+                audioData.Play(0);
+            }
+
+
+            
+
+        }
+    }
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ final echo proejct/Assets/rathin1.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LineRenderer))]
+public class rathin1 : MonoBehaviour
+{
+    // temp2 variable is used to store the distance travelled by the ray.
+    public static float temp2=0;
+    // position variable is used  to get the position value of the component.
+    private Transform position;
+    //ray variable is used to create ray from starting point to the ending point
+    private Ray ray;
+    //valueofray variable is used to get the information from the ray variable when it hits anything.
+    private RaycastHit valueofray;
+    //drawline variable is used to draw the line inorder to trace the path of ray.
+    private LineRenderer drawline;
+    //direction variable is used to get the direction of the ray reflection.
+    private Vector3 direction;
+    //noofreflect varible is consists the number of times the reflection is done.
+    public int noofreflect = 10;
+    // totallength and lineleft variable helps to keep the record of distance travelled by ray when it collides a "playerr". where playerr is a collider
+    private float totallength=2500,lineleft = 2500;
+    
+    void Start()
+    {
+        //getting value of the line renderer attached to the given cylinder(a player).
+        drawline = this.GetComponent<LineRenderer>();
+        //getting value of the position of the player.
+        position = this.GetComponent<Transform>();
+    }
+    void Update()
+    {
+        //on hitting Space key, it willcalculate the distance travelled by ray through various reflection.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //It is used to set value of number of reflection from-------> 1 to noofreflect=10
+            noofreflect = Mathf.Clamp(noofreflect, 1, noofreflect);
+            //setting the starting position of the ray and sending it forward.
+            ray = new Ray(position.position, position.forward);
+            //starting the linerenderer from the origin point.
+            drawline.positionCount = 1;
+            drawline.SetPosition(0, position.position);
+
+            //it will do the reflection until the noofreflect size.
+            //loop breaks if the ray collides with the "playerr" collider.
+            for (int i = 0; i < noofreflect; i++)
+            {
+                //sending the ray if the length does not exceed the max length
+                if (Physics.Raycast(ray.origin, ray.direction, out valueofray, lineleft))
+                {
+                    //calculation the line count
+                    drawline.positionCount= drawline.positionCount+ 1;
+                    //drawing the previous ray line.
+                    drawline.SetPosition(drawline.positionCount - 1, valueofray.point);
+                    //to set reflected point as origin
+                    lineleft = lineleft - Vector3.Distance(ray.origin, valueofray.point);
+                    //generating the ray
+                    ray = new Ray(valueofray.point, Vector3.Reflect(ray.direction, valueofray.normal));
+                   
+                    
+                    //if reflecting ray hits the "playerr" collider then 
+                    //it will  break the loop and store the distance travelled by the ray
+                    //in the temp2 global static variable which can be accessed from other script
+                    if (valueofray.collider.tag == "playerr")
+                    {
+                        print("Ray 2");
+                        //subtracting the distance of ray left from the total distance.
+                        temp2 = totallength - lineleft;
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+}
